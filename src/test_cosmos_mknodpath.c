5237a3440d5735bebdd2fa3a519758e3426cc7fd
Raygan Henley <raygan@raygan.com>
2018-06-02 16:16:35 +0900

    first draft test_cosmos_mknodpath WIP

--- src/test_cosmos_mknodpath.c
+++ src/test_cosmos_mknodpath.c
@@ -1,15 +1,24 @@
+#include "cosmos.h"
 
 
 
 
-
-int main( int argc, char *aegv[] )
+int main( int argc, char *argv[] )
 {
-        struct cosmos _*cm;
-        cosmos_id_t res;
+        struct cosmos *cm;
+        cosmos_id_t cur, proto;
+        char *pathname;
 
-        cm = cosmos_db_init();
+        cm = cosmos_init();
+
+        protoframe = cm->proto; 
+
+        pathname = cosmos_calc_fnpath(cm, "lookup", "cosmos_lookup_fn");
+
+        cur = cosmos_mknod_path(cm, protoframe, 
+
+
+        cosmos_db_cleanup(cm);
 
         return 0;
-
 }
