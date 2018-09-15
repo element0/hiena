9ba63cca1b5b0786024e926fc2a147d1cb003962
Raygan Henley <raygan@raygan.com>
2018-09-16 08:32:51 +0900

    socket test

--- src/experiment/ipc rpc/server.c
+++ src/experiment/ipc rpc/server.c
@@ -1,6 +1,12 @@
-
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
 
+#define SOCKFILEPATH "./testsock"
 
 int daemonize()
 {
@@ -8,9 +14,57 @@ int daemonize()
 }
 
 
+int make_socket()
+{
+	/* socket address */
+
+	struct sockaddr_un sa;
+
+	unlink(SOCKFILEPATH);
+
+	memset(&sa, 0, sizeof(struct sockaddr_un));
+	sa.sun_family = AF_UNIX;
+	snprintf(sa.sun_path, PATH_MAX, SOCKFILEPATH);
+
+	/* socket file descriptor */
+
+	int sfd, cfd;
+
+	sfd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(sfd == -1 )
+	{
+		printf("socket failed.\n");
+		return -1;
+	}
+
+	/* bind socket to address */
+
+	if( bind(sfd, (struct sockaddr *)&sa,
+				sizeof(struct sockaddr_un)) != 0 )
+	{
+		printf("bind failed.\n");
+		return -1;
+	}
+        return 0;
+}
+
+
 int connect()
 {
-    
+	/* listen for connection */
+
+	if( listen(sfd, 1) != 0 )
+	{
+		printf("listen failed.\n");
+		return -1;
+	}
+
+	/* accept connection */
+
+	socklen_t addr_len;
+	cfd = accept(sfd, (struct sockaddr *) &sa, &addr_len);
+
+        return cfd;
 }
 
 
@@ -27,12 +81,17 @@ int main(int argc, char *argv[])
     if(daemonize() == 1)
         return 0;
 
-    make_socket();
+    if(make_socket() == -1)
+    {
+        printf("server: err: make_socket() failed\n");
+        return -1;
+    }
+
 
 
     while(run == 1)
     {
-        connect();
+        cfd = connect();
         do_session();
     }
 
